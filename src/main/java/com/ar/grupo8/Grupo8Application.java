afe0d6ad766481e4ad7c287b1b1cc72950d00faf
rodriramirez6 <rodrigoramirezok@gmail.com>
2024-11-12 15:51:37 -0300

    Subida de proyecto

---
+++ src/main/java/com/ar/grupo8/Grupo8Application.java
@@ -0,0 +1,13 @@
+package com.ar.grupo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/grupo8/Grupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.grupo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
