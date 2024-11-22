23d027b8ce9f2ec1cf7af69a3a51fb2640788234
rodriramirez6 <rodrigoramirezok@gmail.com>
2024-11-22 00:11:34 -0300

    Se importaron algunas dependencias en UsuarioEmpresa

--- src/main/java/com/ar/grupo8/models/UsuarioEmpresa.java
+++ src/main/java/com/ar/grupo8/models/UsuarioEmpresa.java
@@ -1,4 +1,11 @@
 package com.ar.grupo8.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data // Genera automaticamente los Getters y Setters sin que tengas que escribirlo manualmente.
+@Entity // Marca una clase como una entidad de JPA, lo que significa que esta clase se va a mapear a una tabla en la base de datos.
+@Table // Especifica el nombre de la tabla en la base de datos a la que se va a mapear la entidad.
 public class UsuarioEmpresa {
+
 }
